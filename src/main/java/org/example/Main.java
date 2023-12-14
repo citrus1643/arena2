40dabd8ab8331cfa12abbbbbfdc9290b411898dc
headline1643 <s1ngl3tolenov@gmail.com>
2023-12-14 22:02:13 +0600

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,53 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //1 задача
+        for(int i=0;i<2;i++){
+            int a = 0;
+            int b = a*a;
+            int c = 1;
+            int d = c*c;
+            int e = 2;
+            int f = e*e;
+            int g = 3;
+            int h = g*g;
+            int j = 4;
+            int k = j*j;
+            int l = 5;
+            int m = l*l;
+            int n = 6;
+            int o = n*n;
+            int p = 7;
+            int q = p*p;
+            int r = 8;
+            int s = r*r;
+            int t = 9;
+            int u = t*t;
+            int y = 10;
+            int x = y*y;
+            System.out.println(b + " " + d + " " + f + " " + h + " " + k + " " + m + " " + o + " " + q + " " + s + " " + u + " " + x);
+        }
+        //Изначальный вариант решения 2й задачи до того как увидел, что n должен быть целочисленным
+        //char y = 1000;
+        //char x = 31;
+        //while(y>=x){
+        //    x++;
+        //    System.out.println(x);
+        //}
+        int n = 1000;
+        int m = 32;
+        while(n>=m){
+            char c = (char) m;
+            System.out.println(c);
+            m++;
+        }
+        //3 задача
+        String message = "Hello World";
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+            int ascii = (int) c;
+            System.out.println("Символ: " + c + "  Код ASCII: " + ascii);
+        }
+    }
+}
